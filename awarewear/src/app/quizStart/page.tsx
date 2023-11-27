27357e09e8a216a0ffdcdddd1327c652ca5a41f4
npaik <nakhyunpaik@gmail.com>
2023-11-27 14:23:07 -0800

    quiz

--- awarewear/src/app/quizStart/page.tsx
+++ awarewear/src/app/quizStart/page.tsx
@@ -1,72 +1,137 @@
 "use client";
 import React, { useState } from "react";
 import QuestionButton from "@/components/QuestionButton";
-
-interface QuizQuestion {
-  question: string;
-  options: string[];
-  correctAnswer: string;
-}
+import quizData from "./fastFashionQuiz.json";
 
 export default function QuizStart() {
-  const quizData: QuizQuestion[] = [
-    {
-      question: "What is the primary goal of fast fashion?",
-      options: [
-        "Create durable clothing",
-        "Promote sustainable practices",
-        "Produce clothing quickly and cheaply",
-        "Increase the use of organic materials",
-      ],
-      correctAnswer: "Produce clothing quickly and cheaply",
-    },
-    // ... other questions if you have more
-  ];
-
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
-  const currentQuestion = quizData[0];
+  const [isWrongAnswer, setIsWrongAnswer] = useState<boolean>(false);
+  const [correctAnswers, setCorrectAnswers] = useState(0); // Tracks the number of correct answers
+  const [quizComplete, setQuizComplete] = useState(false); // Tracks if the quiz is complete
+
+  const currentQuestion = quizData[currentQuestionIndex];
 
   const handleAnswerClick = (option: string) => {
     setSelectedAnswer(option);
-    if (option === currentQuestion.correctAnswer) {
-      // Logic for correct answer
+    const isCorrect = option === currentQuestion.answer;
+    setIsWrongAnswer(!isCorrect);
+
+    if (isCorrect) {
+      setCorrectAnswers(correctAnswers + 1); // Increment only if the answer is correct
     }
+
+    // Move to the next question after a delay whether the answer is correct or not
+    setTimeout(() => {
+      if (currentQuestionIndex < quizData.length - 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setSelectedAnswer(null); // Reset selected answer for next question
+        setIsWrongAnswer(false); // Reset wrong answer state
+      } else {
+        setQuizComplete(true); // End of quiz
+      }
+    }, 500);
   };
+
+  const handleTryAgain = () => {
+    // Reset the quiz
+    setCurrentQuestionIndex(0);
+    setSelectedAnswer(null);
+    setIsWrongAnswer(false);
+    setCorrectAnswers(0);
+    setQuizComplete(false);
+  };
+
+  if (quizComplete) {
+    // Calculate the score percentage
+    const scorePercent = (correctAnswers / quizData.length) * 100;
+    return (
+      <div
+        className="results-container"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh", // Use the full height of the viewport
+        }}
+      >
+        <h1>Quiz Results</h1>
+        <div
+          className="results-text"
+          style={{ marginTop: "20px", fontSize: "24px" }}
+        >
+          {scorePercent >= 80
+            ? "Congratulations!"
+            : "Failed. Please try again!"}
+        </div>
+        <div className="score" style={{ marginTop: "20px", fontSize: "20px" }}>
+          Your Score: {scorePercent}%
+        </div>
+        <div
+          className="passing-score"
+          style={{ marginTop: "20px", fontSize: "20px" }}
+        >
+          Passing Score: 80%
+        </div>
+        <button
+          onClick={handleTryAgain}
+          style={{
+            marginTop: "30px",
+            padding: "10px 20px",
+            fontSize: "20px",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Try Again
+        </button>
+      </div>
+    );
+  }
 
   return (
     <>
       <main className="relative w-full h-screen bg-no-repeat bg-cover overflow-hidden">
-        {/* Background image with reduced opacity */}
-        <div className="absolute top-0 left-0 w-full h-full bg-cover bg-[url('/quiz-bg.png')] bg-center opacity-30"></div>
-
-        {/* Content with no opacity */}
+        {/* Background image and content code */}
         <div className="relative z-10 flex flex-col items-center justify-center h-full space-y-4">
-          {/* Assuming the question button is not meant to be clicked, it doesn't have onClick */}
           <QuestionButton
             questionText={currentQuestion.question}
             href=""
             className="text-2xl font-bold text-center text-blue-800 bg-transparent"
           />
 
-          {currentQuestion.options.map((option, index) => {
-            const isSelected = option === selectedAnswer;
-            const isCorrectAnswer = option === currentQuestion.correctAnswer;
-            const buttonStyles = isSelected
-              ? isCorrectAnswer
-                ? "bg-green-500" // Correct selected answer
-                : "bg-red-500" // Incorrect selected answer
-              : "bg-blue-500"; // Not selected answer
+          {Object.entries(currentQuestion.options).map(
+            ([key, value], index) => {
+              const isSelected = key === selectedAnswer;
+              const isCorrectAnswer = key === currentQuestion.answer;
+              let buttonStyles = "bg-blue-500"; // Default style
 
-            return (
-              <QuestionButton
-                key={index}
-                questionText={option}
-                onClick={() => handleAnswerClick(option)}
-                className={`px-6 py-3 rounded-full text-white font-semibold ${buttonStyles}`}
-                href=""
-              />
-            );
-          })}
+              if (isSelected) {
+                buttonStyles = isCorrectAnswer ? "bg-green-500" : "bg-red-500";
+              }
+
+              return (
+                <div key={index} className="relative">
+                  <QuestionButton
+                    questionText={value}
+                    onClick={() => handleAnswerClick(key)}
+                    className={`px-6 py-3 rounded-full text-white font-semibold ${buttonStyles}`}
+                    href=""
+                  />
+                  {isSelected && (
+                    <div
+                      className={`absolute -right-20 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-lg shadow-lg ${
+                        isWrongAnswer ? "text-red-500" : "text-green-500"
+                      }`}
+                    >
+                      {isWrongAnswer ? "Wrong Answer" : "Correct!"}
+                    </div>
+                  )}
+                </div>
+              );
+            }
+          )}
         </div>
       </main>
     </>
